c9fe9d54d287c057e00d1f3066924d7f9d9e98ee
Ed Colen <ed.colen@gmail.com>
2021-06-21 15:31:59 -0300

    added inclusion validation

--- app/models/box_item.rb
+++ app/models/box_item.rb
@@ -1,4 +1,11 @@
 class BoxItem < ApplicationRecord
-  validates :box_name, presence: true
+  BOX_NAMES = [
+    'Happy Hour',
+    'Beleza e Cuidado',
+    'Receita Certa'
+  ].freeze
+
+  validates :box_name, presence: true,
+                       inclusion: { in: BOX_NAMES }
   validates :item_name, presence: true
 end
