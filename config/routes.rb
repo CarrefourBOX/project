9dd070e96e75ac464b7fbcbe09d96a465daf2af5
Ed Colen <ed.colen@gmail.com>
2021-06-22 18:22:37 -0300

    added plans routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-
   get 'dashboard', to: 'pages#dashboard', as: :dashboard
+  get 'my_boxes', to: 'pages#my_boxes', as: :my_boxes
 
+  resources :plans, only: %i[new create]
   resources :box_names, only: %i[create destroy]
   resources :box_items, only: %i[create destroy]
 end
