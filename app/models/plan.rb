0113cd8dc990a84cd0b19c3f958100c2f77f47be
guilherme fontana <guifl2001@hotmail.com>
2021-06-25 10:03:18 -0300

    create full_price in plans

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -17,6 +17,7 @@ class Plan < ApplicationRecord
   before_validation :calculate_price, :calculate_shipment, :calculate_expiration, :set_ship_day
 
   monetize :price_cents, as: 'price'
+  monetize :full_price_cents, as: 'full_price'
   monetize :shipment_cents, as: 'shipment'
 
   validates :category, presence: true,
@@ -28,13 +29,20 @@ class Plan < ApplicationRecord
   validates :carrefour_card, inclusion: { in: [true, false] }
   validates :auto_renew, inclusion: { in: [true, false] }
   validates :quantity, presence: true
+  validates :full_price_cents, presence: true
 
   private
 
   def calculate_price
-    total_price = (CATEGORIES[category][0] * CATEGORIES[category][1]) * quantity
+    total_price = (CATEGORIES[category][0]) * quantity
     discounts = quantity == 1 ? 0 : (total_price * DISCOUNTS[quantity]) / 100
     self.price_cents = total_price - discounts
+  end
+
+  def calculate_price
+    total_price = (CATEGORIES[category][0] * CATEGORIES[category][1]) * quantity
+    discounts = quantity == 1 ? 0 : (total_price * DISCOUNTS[quantity]) / 100
+    self.full_price_cents = total_price - discounts
   end
 
   def calculate_shipment
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_25_005148) do
+ActiveRecord::Schema.define(version: 2021_06_25_124202) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -96,6 +96,7 @@ ActiveRecord::Schema.define(version: 2021_06_25_005148) do
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "payment", default: false
     t.datetime "expires_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.integer "mensal_plan_price"
     t.index ["user_id"], name: "index_plans_on_user_id"
   end
 
