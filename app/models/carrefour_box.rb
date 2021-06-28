21ed4e547679fa36c9441e97115ab25c37115349
Ed Colen <ed.colen@gmail.com>
2021-06-28 10:41:47 -0300

    added dependent destroy to box_item association

--- app/models/carrefour_box.rb
+++ app/models/carrefour_box.rb
@@ -2,6 +2,6 @@ class CarrefourBox < ApplicationRecord
   validates :name, :description, :color, presence: true
   validates :name, uniqueness: true
 
-  has_many :box_items
+  has_many :box_items, dependent: :destroy
   has_one_attached :icon
 end
