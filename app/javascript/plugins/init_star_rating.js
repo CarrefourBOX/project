754fb9f3881d920e6b1ba25190e5d06a900b2ee6
guilherme fontana <guifl2001@hotmail.com>
2021-06-29 16:41:49 -0300

    everything done except stars

--- app/javascript/plugins/init_star_rating.js
+++ app/javascript/plugins/init_star_rating.js
@@ -1,5 +1,5 @@
-import "jquery-bar-rating";
 import $ from 'jquery'; // <-- if you're NOT using a Le Wagon template (cf jQuery section)
+import "jquery-bar-rating";
 
 const initStarRating = () => {
   $('#review_rating').barrating({
