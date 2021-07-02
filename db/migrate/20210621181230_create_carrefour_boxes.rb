0311aeffb0d9bbd658c2fb3f16785c5cec362f98
Ed Colen <ed.colen@gmail.com>
2021-07-01 22:43:35 -0300

    added summary for carrefour_boxes

--- db/migrate/20210621181230_create_carrefour_boxes.rb
+++ db/migrate/20210621181230_create_carrefour_boxes.rb
@@ -3,6 +3,7 @@ class CreateCarrefourBoxes < ActiveRecord::Migration[6.1]
     create_table :carrefour_boxes do |t|
       t.string :name
       t.string :color, null: false, default: ''
+      t.text :summary, null: false, default: ''
       t.text :description, null: false, default: ''
       t.jsonb :plans, null: false, default: {}
       t.float :average_rating, precision: 2, scale: 1
