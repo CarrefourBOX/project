2be46f80ce1576742b5143991c9ce98275c4d88c
Ed Colen <ed.colen@gmail.com>
2021-07-01 23:12:45 -0300

    updated box show

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,15 +3,25 @@ class Review < ApplicationRecord
   belongs_to :carrefour_box
 
   after_create :update_carrefour_box_rating
+  before_destroy :update_carrefour_box_rating_destroy
 
   validates :rating, presence: true, inclusion: { in: (1..5).to_a }, numericality: { only_integer: true }
 
   private
 
   def update_carrefour_box_rating
-    new_rating = carrefour_box.reviews.reduce(0.0) do |total, review|
+    reviews = carrefour_box.reviews
+    new_rating = reviews.reduce(0.0) do |total, review|
       review.rating + total
-    end.to_f / carrefour_box.reviews.size
+    end.to_f / reviews.size
+    carrefour_box.update(average_rating: new_rating.round(1))
+  end
+
+  def update_carrefour_box_rating_destroy
+    reviews = carrefour_box.reviews.where.not(id: id)
+    new_rating = reviews.reduce(0.0) do |total, review|
+      review.rating + total
+    end.to_f / reviews.size
     carrefour_box.update(average_rating: new_rating.round(1))
   end
 end
